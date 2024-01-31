786c1440ea7072b9045ee36aece7b50a593ac6f8
m-tsukada <tskd0079@gmail.com>
2024-01-31 20:28:06 +0900

    入力チェック機能の実装

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.techacademy.controller;
 
+import java.util.Set; // 追加
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.User;
 import com.techacademy.service.UserService;
@@ -28,24 +33,28 @@ public class UserController {
         // user/list.htmlに画面遷移
         return "user/list";
     }
-    
-    /** User登録機能を表示 */
+
+    /** User登録画面を表示 */
     @GetMapping("/register")
     public String getRegister(@ModelAttribute User user) {
-        //User登録画面に遷移
+        // User登録画面に遷移
         return "user/register";
     }
-    
+
     /** User登録処理 */
     @PostMapping("/register")
-    public String postRegister(User user) {
+    public String postRegister(@Validated User user, BindingResult res, Model model) {
+        if(res.hasErrors()) {
+            //エラーあり
+            return getRegister(user);
+        }
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
-    
-    /** User 更新画面を表示 */
+
+    /** User更新画面を表示 */
     @GetMapping("/update/{id}/")
     public String getUser(@PathVariable("id") Integer id, Model model) {
         // Modelに登録
@@ -53,13 +62,24 @@ public class UserController {
         // User更新画面に遷移
         return "user/update";
     }
-    
-    // User更新処理 */
-    @PostMapping("/update/{jd}/")
+
+    /** User更新処理 */
+    @PostMapping("/update/{id}/")
     public String postUser(User user) {
         // User登録
         service.saveUser(user);
         // 一覧画面にリダイレクト
         return "redirect:/user/list";
     }
+
+    // ----- 追加:ここから -----
+    /** User削除処理 */
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRun(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        // Userを一括削除
+        service.deleteUser(idck);
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
+    // ----- 追加:ここまで -----
 }
